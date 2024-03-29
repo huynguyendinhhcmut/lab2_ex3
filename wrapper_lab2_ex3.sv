ad98bf89d17ac71e0ec755699612db5e6c7a28e9
huynguyendinhhcmut <151980955+huynguyendinhhcmut@users.noreply.github.com>
2024-03-29 07:05:51 +0700

    Add files via upload

--- wrapper_lab2_ex3.sv
+++ wrapper_lab2_ex3.sv
@@ -6,6 +6,6 @@ module wrapper_lab2_ex3 (
 
 assign LEDR[0] = SW[0];
 
-lab2_ex3 lab2ex3 ( .X(SW[9]), .rst(SW[0]), .clk50M(CLOCK_50), .L1(LEDR[1]), .L2(LEDR[2]), .L3(LEDR[3]), .L4(LEDR[4]), .L5(LEDR[5]), .L6(LEDR[6]), .L7(LEDR[7]), .L8(LEDR[8]), .Z(LEDR[9]));
+lab2_ex3 lab2ex3 ( .X(SW[9]), .rst(SW[0]), .clk50M(CLOCK_50), .L1(LEDR[1]), .L2(LEDR[2]), .L3(LEDR[3]), .L4(LEDR[4]), .L5(!LEDR[5]), .L6(!LEDR[6]), .L7(!LEDR[7]), .L8(!LEDR[8]), .Z(LEDR[9]));
 
 endmodule
